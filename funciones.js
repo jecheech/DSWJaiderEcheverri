b6659ee49e75a75072cb9719ac4b41dd7ce9f1a8
jecheech <jaiderstiven@gmail.com>
2020-09-18 17:24:56 -0500

    final
    final

---
+++ funciones.js
@@ -0,0 +1,10 @@
+
+
+function tabs(bloques) {
+  var i;
+  var x = document.getElementsByClassName("contenido");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(bloques).style.display = "block";  
+}
